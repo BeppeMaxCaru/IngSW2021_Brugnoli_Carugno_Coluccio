b7631b567e9ec38905673508727208de555be9ae
BeppeMaxCaru <gmaxcarugno@gmail.com>
2021-04-28 22:19:55 +0200

    Implementing the server

--- src/main/java/org/example/EchoServerClientHandler.java
+++ src/main/java/org/example/EchoServerClientHandler.java
@@ -1,28 +1,52 @@
 package org.example;
 
+import Maestri.MVC.Model.GModel.GamePlayer.Player;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
 import java.util.Scanner;
 
 public class EchoServerClientHandler implements Runnable {
     private Socket socket;
+
+    private Player player;// = new Player();
+    //this.player.chooseNickname();
+
     public EchoServerClientHandler(Socket socket) {
         this.socket = socket;
+        this.player = new Player();
+        this.player.chooseNickname();
     }
+
+    @Override
     public void run() {
         try {
             Scanner in = new Scanner(socket.getInputStream());
             PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            Player player = new Player();
+            try {
+                player.chooseNickname();
+            } catch (Exception e) {
+                e.printStackTrace();
+                out.println("You weren't able to join a game");
+            }
+
 // Leggo e scrivo nella connessione finche' non ricevo "quit"
             while (true) {
-                String line = in.nextLine();
+                this.player.getLeaderAction();
+                this.player.getAction();
+                this.player.getLeaderAction();
+
+                /*String line = in.nextLine();
                 if (line.equals("quit")) {
                     break;
                 } else {
                     out.println("Received: " + line);
                     out.flush();
-                }
+                }*/
             }
 // Chiudo gli stream e il socket
             in.close();
--- src/main/java/org/example/MultiEchoServer.java
+++ src/main/java/org/example/MultiEchoServer.java
@@ -1,18 +1,32 @@
 package org.example;
 
+import Maestri.MVC.Model.GModel.GameModel;
+import Maestri.MVC.Model.GModel.GamePlayer.Player;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MultiEchoServer {
+    //Connection
     private int port;
     public MultiEchoServer(int port) {
         this.port = port;
     }
+
+    //Clients handling
+    private List<EchoServerClientHandler> clients = new ArrayList<>();
+
+    //GameModel
+    private GameModel game1;
+
     public void startServer() {
-        ExecutorService executor = Executors.newCachedThreadPool();
+        //4 threads for 4 players
+        ExecutorService executor = Executors.newFixedThreadPool(4);
         ServerSocket serverSocket;
         try {
             serverSocket = new ServerSocket(port);
@@ -20,17 +34,25 @@ public class MultiEchoServer {
             System.err.println(e.getMessage()); // Porta non disponibile
             return;
         }
-        System.out.println("Server ready");
+        System.out.println("Server ready for Masters of Renaissance");
         while (true) {
             try {
                 Socket socket = serverSocket.accept();
-                executor.submit(new EchoServerClientHandler(socket));
+                EchoServerClientHandler newClient = new EchoServerClientHandler(socket);
+                executor.submit(newClient);
+                //Better to make the thread do everything!
+                this.clients.add(newClient);
+                //Modificare costruttore player -> togliere nickName
+                //Player player = new Player();
+                //player.chooseNickname();
+                //executor.execute();
             } catch(IOException e) {
                 break; // Entrerei qui se serverSocket venisse chiuso
             }
         }
         executor.shutdown();
     }
+
     public static void main(String[] args) {
         MultiEchoServer echoServer = new MultiEchoServer(1234);
         echoServer.startServer();
