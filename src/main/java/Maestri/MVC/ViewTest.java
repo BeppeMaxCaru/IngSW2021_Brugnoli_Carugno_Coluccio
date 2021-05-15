f5e9bc5a6198ca6f9b20b5252a97e6ce1184f93b
BeppeMaxCaru <gmaxcarugno@gmail.com>
2021-05-15 15:14:24 +0200

    Fixed JavaFX pom

--- src/main/java/Maestri/MVC/ViewTest.java
+++ src/main/java/Maestri/MVC/ViewTest.java
@@ -1,5 +1,29 @@
 package Maestri.MVC;
 
-public class ViewTest {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class ViewTest extends Application {
     //contiene tutto il gamemodel ma modifica solo quello che gli viene notificato
+    @Override
+    public void start(Stage stage) {
+        var javaVersion = System.getProperty("java.version");
+        var javafxVersion = System.getProperty("javafx.version");
+
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion);
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module Maestri.MVC {
+    requires javafx.controls;
+    exports Maestri.MVC;
+        }
