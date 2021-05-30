6bfd8a6780e1f3481595bba5fa39af10e01a22d3
alicebrugnoli <alice.brugnoli@mail.polimi.it>
2021-05-30 12:51:38 +0200

    gui - starting resources

--- src/main/java/Communication/ClientSide/RenderingView/GUI.java
+++ src/main/java/Communication/ClientSide/RenderingView/GUI.java
@@ -12,16 +12,20 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class GUI extends Application implements RenderingView {
 
     String nickname;
+    int playerNumber;
 
     public static void main(String[] args) {
        /*
@@ -89,7 +93,8 @@ public class GUI extends Application implements RenderingView {
 
     @Override
     public void start(Stage stage) {
-        nick(stage);
+        startingResources(stage);
+        //nick(stage);
     }
 
     //@Override
@@ -118,7 +123,7 @@ public class GUI extends Application implements RenderingView {
 
         Label lbl = new Label("Insert nickname:");
         TextField field = new TextField();
-        Button okBtn = new Button("Start game");
+        Button okBtn = new Button("Ok");
 
         okBtn.setOnAction(e -> {
             multiOrSinglePlayers(stage);
@@ -147,9 +152,17 @@ public class GUI extends Application implements RenderingView {
         ToggleButton button1 = new ToggleButton("Single player");
         ToggleButton button2 = new ToggleButton("Multi player");
         root.add(button1, 0, 0);
-        root.add(button2, 0, 1);
+        root.add(button2, 1, 0);
 
         Scene scene = new Scene(root, 300, 100);
+
+        /* button1.setOnAction(e -> {
+            single player welcome
+        }); */
+
+        button2.setOnAction(e -> {
+            welcome(stage);
+        });
 
         stage.setTitle("Multi Or Single Players?");
         stage.setScene(scene);
@@ -161,19 +174,17 @@ public class GUI extends Application implements RenderingView {
 
     public void welcome(Stage stage) {
         addLabelByCode(stage, "Loading...\nHi " + this.nickname + "!\nWelcome to Master of Renaissance online!");
-        LoadWTFOnTimer(stage, "welcome");
+        matchHasStarted(stage, 0); // da togliere
     }
 
     public void matchHasStarted(Stage stage, int playerNumber) {
-        addLabelByCode(stage, "Match has started, your player number is " + playerNumber);
+        // Chiamata dal client? Devo far apparire questo messaggio quando il server si connette alla socket.
+        this.playerNumber = playerNumber;
+        addLabelByCode(stage, "Match has started, your player number is " + this.playerNumber);
+        LoadWTFOnTimer(stage);
     }
 
-    public void startingResources(Stage stage) {
-        stage.setTitle("Pick initial resources");
-        Group root = new Group();
-        Canvas canvas = new Canvas(730, 150);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
+    public String startingResources(Stage stage) {
         String[] resources =  new String[] {
                 "coin.png",
                 "servant.png",
@@ -181,17 +192,118 @@ public class GUI extends Application implements RenderingView {
                 "stone.png"
         };
 
+/*
         int x = 10;
         for(String item: resources) {
             Image img = new Image(item);
+            ImageView view = new ImageView(img);
+            view.setFitHeight(80);
+            view.setPreserveRatio(true);
+            ToggleButton button = new ToggleButton();
+            button.setTranslateX(x);
+            button.setTranslateY(20);
+            button.setPrefSize(80, 80);
+            button.setGraphic(view);
             gc.drawImage( img, x, 20, 100, 100 );
             x+=200;
         }
+/*
+        //Image coinImg = new Image("coin.png");
+        ToggleButton button = new ToggleButton();
+        coinButton.setGraphic(new ImageView("coin.png"));
+        //Image servantImg = new Image("servant.png");
+        ToggleButton servantButton = new ToggleButton();
+        coinButton.setGraphic(new ImageView("servant.png"));
+
+        //Image shieldImg = new Image("shield.png");
+        ToggleButton shieldButton = new ToggleButton();
+        coinButton.setGraphic(new ImageView("shield.png"));
+
+        //Image stoneImg = new Image("stone.png");
+        ToggleButton stoneButton = new ToggleButton();
+        coinButton.setGraphic(new ImageView( "stone.png"));
+
 
         root.getChildren().add(canvas);
         stage.setScene(new Scene(root));
 
         stage.show();
+
+        /*if(button.isSelected()) {
+            System.out.println("ciao");
+            return "COINS";
+        }*/
+
+        Group root = new Group();
+        //Creating buttons
+        ToggleButton coinButton = new ToggleButton();
+        ToggleButton servantButton = new ToggleButton();
+        ToggleButton shieldButton = new ToggleButton();
+        ToggleButton stoneButton = new ToggleButton();
+
+        int x = 10;
+        for(String item: resources) {
+            //Creating a graphic (image)
+            Image img = new Image(item);
+            ImageView view = new ImageView(img);
+            view.setFitHeight(80);
+            view.setPreserveRatio(true);
+            //Setting the location of the button
+            switch (item) {
+                case "coin.png":
+                    coinButton.setTranslateX(x);
+                    coinButton.setTranslateY(20);
+                    //Setting the size of the button
+                    coinButton.setPrefSize(80, 80);
+                    //Setting a graphic to the button
+                    coinButton.setGraphic(view);
+                    x = x + 200;
+                    root.getChildren().add(coinButton);
+                    break;
+                case "servant.png":
+                    servantButton.setTranslateX(x);
+                    servantButton.setTranslateY(20);
+                    //Setting the size of the button
+                    servantButton.setPrefSize(80, 80);
+                    //Setting a graphic to the button
+                    servantButton.setGraphic(view);
+                    x = x + 200;
+                    root.getChildren().add(servantButton);
+                    break;
+                case "shield.png":
+                    shieldButton.setTranslateX(x);
+                    shieldButton.setTranslateY(20);
+                    //Setting the size of the button
+                    shieldButton.setPrefSize(80, 80);
+                    //Setting a graphic to the button
+                    shieldButton.setGraphic(view);
+                    x = x + 200;
+                    root.getChildren().add(shieldButton);
+                    break;
+                default:
+                    stoneButton.setTranslateX(x);
+                    stoneButton.setTranslateY(20);
+                    //Setting the size of the button
+                    stoneButton.setPrefSize(80, 80);
+                    //Setting a graphic to the button
+                    stoneButton.setGraphic(view);
+                    x = x + 200;
+                    root.getChildren().add(stoneButton);
+                    break;
+            }
+        }
+
+        //Setting the stage
+        Scene scene = new Scene(root, 740, 130);
+        stage.setTitle("Button Graphics");
+        stage.setScene(scene);
+        stage.show();
+
+        if(coinButton.isSelected()) {
+            System.out.println("ciao");
+        }
+
+        return "c";
     }
 
     public void addLabelByCode(Stage stage, String string) {
@@ -202,7 +314,7 @@ public class GUI extends Application implements RenderingView {
         stage.show();
     }
 
-    static void LoadWTFOnTimer(Stage stage, String nameMethod) {
+    public void LoadWTFOnTimer(Stage stage) {
         TimerTask task = new TimerTask() {
 
             public void run() {
@@ -210,7 +322,7 @@ public class GUI extends Application implements RenderingView {
                 Platform.runLater(() -> {
                     try {
                         System.out.println("loading..");
-
+                        startingResources(stage);
 
                     } catch (Exception e) {
                         System.out.println(e);
@@ -221,7 +333,7 @@ public class GUI extends Application implements RenderingView {
 
 
         Timer timer = new Timer("Timer");
-        long delay = 5000L;
+        long delay = 1000L;
         timer.schedule(task, delay);
     }
 }
